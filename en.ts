7293ec0c1ba95da6aab426348f4896116f0c66f6
insraq <ruoysun@gmail.com>
2021-12-04 22:25:25 +0200

    Update

--- en.ts
+++ en.ts
@@ -393,8 +393,6 @@ export const EN = {
     WelcomePlay: "Play",
     WelcomePlayMuted: "Play (🔇Music)",
     Chat: "Chat",
-    Import: "Import",
-    Export: "Export",
     RestoreFromBackup: "Restore from Backup",
     RestoreFromBackupFail: "Failed to restore backup from Steam Cloud: are you sure there's a backup?",
     RestoreFromBackupTitle: "Are you sure?",
@@ -1120,4 +1118,7 @@ export const EN = {
     MetaRebrandingDesc:
         "Social Network Inc is renamed to Meta Inc and consumes internet, game, super computer, bitcoin and dogecoin instead",
     MetaInc: "Meta Inc",
+    ImportSave: "Import Save",
+    ExportSave: "Export Save",
+    ClearMyTrades: "Clear My Trades",
 };
